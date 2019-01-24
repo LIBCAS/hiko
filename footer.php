c5511dab9a909d77eda478502fa88217abae5da9
JarkaP <jarka.p@gmail.com>
2019-01-24 17:11:41 +0100

    add uppy links

--- footer.php
+++ footer.php
@@ -5,6 +5,7 @@
     <script src="https://cdn.jsdelivr.net/npm/vue-tables-2@1.4.70/dist/vue-tables-2.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/slim-select@1.18.6/dist/slimselect.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@8"></script>
+    <script src="https://transloadit.edgly.net/releases/uppy/v0.29.1/dist/uppy.min.js"></script>
     <script src="<?= get_template_directory_uri(); ?>/assets/dist/custom.min.js?v=<?= filemtime(get_template_directory() . '/assets/dist/custom.min.js'); ?>"></script>
 
     <?php wp_footer(); ?>
--- header.php
+++ header.php
@@ -9,6 +9,7 @@
     <link rel="stylesheet" href="https://bootswatch.com/4/sandstone/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/slim-select@1.18.6/dist/slimselect.min.css">
     <link href="<?= get_template_directory_uri(); ?>/assets/open-iconic/font/css/open-iconic-bootstrap.min.css" rel="stylesheet">
+        <link href="https://transloadit.edgly.net/releases/uppy/v0.29.1/dist/uppy.min.css" rel="stylesheet">
     <link rel="stylesheet" href="<?= get_template_directory_uri() . '/assets/dist/main.min.css?ver=' . filemtime(get_template_directory() . '/assets/dist/main.min.css'); ?>">
     <script type="text/javascript">
         var ajaxUrl = '<?= admin_url('admin-ajax.php'); ?>';
