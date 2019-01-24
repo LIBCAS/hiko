ce9c04af317c783144383fcf77582237422df805
JarkaP <jarka.p@gmail.com>
2019-01-24 18:05:54 +0100

    Update blekastad-images.php

--- partials/blekastad-images.php
+++ partials/blekastad-images.php
@@ -1,7 +1,33 @@
-<div class="" id="media-handler">
+<?php
+$error = false;
 
-    <h3>Nahrát obrazové přílohy</h3>
-    <div id="drag-drop-area" class="mb-5"></div>
+if (!array_key_exists('letter', $_GET)) {
+    $error = true;
+} else {
+    $pod = pods('bl_letter', $_GET['letter']);
 
-    <h3>Vybrat veřejné obrazové přílohy</h3>
-</div>
+    if (!$pod->exists()) {
+        $error = true;
+    }
+}
+
+?>
+
+
+<?php if ($error) : ?>
+    <div class="alert alert-warning">
+        Nepodařilo se načíst požadovaný dopis.
+    </div>
+<?php else : ?>
+    <div class="" id="media-handler">
+        <h3>Dopis: <?= $pod->field('name'); ?></h3>
+        <div class="section mb-5">
+            <h4>Nahrát obrazové přílohy</h4>
+            <div id="drag-drop-area"></div>
+        </div>
+        <div class="section mb-5">
+            <h4>Upravit nahrané obrazové přílohy</h4>
+
+        </div>
+    </div>
+<?php endif; ?>
