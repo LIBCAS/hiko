85a0bf9a9907034479f7b080ee93fbc47453f206
JarkaP <jarka.p@gmail.com>
2019-02-07 12:35:14 +0100

    create preview template

--- page-templates/page-letter-preview.php
+++ page-templates/page-letter-preview.php
@@ -9,6 +9,13 @@ if (array_key_exists('l_type', $_GET) && array_key_exists('letter', $_GET)) {
     $letter_id = sanitize_text_field($_GET['letter']);
     $letter_type = sanitize_text_field($_GET['l_type']);
     $pod = pods($letter_type, $letter_id);
+    if ($letter_type == 'bl_letter') {
+        $link_dashboard = home_url('/blekastad/letters/');
+        $link_letter_edit = home_url('/blekastad/letters-add/?edit=' . $letter_id);
+    } else {
+        $link_dashboard = '#';
+        $link_letter_edit = '#';
+    }
 } else {
     die('Nepodařilo se načíst požadovaný dopis');
 }
@@ -34,10 +41,10 @@ if (!$pod->exists()) {
     </head>
     <body>
         <div class="container-fluid">
-            <div id="letter-preview" class="row main-content mb-5">
+            <div id="letter-preview" class="row main-content my-5">
                 <div class="col-md-9">
                     <div class="letter-single">
-                        <h3>22. 4. 1999:  Klaus Schaller (Bochum)  →  Milada Blekastad (Oslo)</h3>
+                        <h3> {{ title }}</h3>
                         <div class="my-5">
                             <h5>Data</h5>
                             <table class="table">
@@ -47,7 +54,19 @@ if (!$pod->exists()) {
                                             Datum dopisu
                                         </td>
                                         <td>
-                                            22. 4. 1999
+                                            {{ day ? day : '?' }}. {{ month ? month : '?'}}. {{ year ? year : '????' }}
+                                            <span v-if="date_uncertain">
+                                                <br>
+                                                <small>(Nejisté datum)</small>
+                                            </span>
+                                        </td>
+                                    </tr>
+                                    <tr v-if="date_marked">
+                                        <td style="width: 20%;">
+                                            Datum uvedené v dopise
+                                        </td>
+                                        <td>
+                                            {{ date_marked }}
                                         </td>
                                     </tr>
                                     <!---->
@@ -61,15 +80,53 @@ if (!$pod->exists()) {
                                     <tr>
                                         <td style="width: 20%;">Autor</td>
                                         <td>
-                                            <span>Klaus Schaller</span>
+                                            <span v-for="a in author" class="d-block"> {{ a }}</span>
+                                            <span v-if="author_uncertain" class="d-block">
+                                                <small>(Nejistý autor)</small>
+                                            </span>
+                                            <span v-if="author_inferred" class="d-block">
+                                                <small>(Autor vyvozený z obsahu dopisu)</small>
+                                            </span>
                                         </td>
                                     </tr>
-                                    <tr>
-                                        <td>Příjemce</td>
+                                    <tr v-if="author_as_marked">
+                                        <td style="width: 20%;">
+                                            Autor uvedený v dopise
+                                        </td>
                                         <td>
-                                            <span>Milada Blekastad</span>
+                                            {{ author_as_marked }}
                                         </td>
                                     </tr>
+
+                                    <tr>
+                                        <td style="width: 20%;">Příjemce</td>
+                                        <td>
+                                            <span v-for="r in recipient" class="d-block"> {{ r }}</span>
+                                            <span v-if="recipient_uncertain" class="d-block">
+                                                <small>(Nejistý příjemce)</small>
+                                            </span>
+                                            <span v-if="recipient_inferred" class="d-block">
+                                                <small>(Příjemce vyvozený z obsahu dopisu)</small>
+                                            </span>
+                                        </td>
+                                    </tr>
+                                    <tr v-if="recipient_marked">
+                                        <td style="width: 20%;">
+                                            Příjemce uvedený v dopise
+                                        </td>
+                                        <td>
+                                            {{ recipient_marked }}
+                                        </td>
+                                    </tr>
+                                    <tr v-if="recipient_notes">
+                                        <td style="width: 20%;">
+                                            Poznámky o příjemci
+                                        </td>
+                                        <td>
+                                            {{ recipient_notes }}
+                                        </td>
+                                    </tr>
+
                                 </tbody>
                             </table>
                         </div>
@@ -80,13 +137,38 @@ if (!$pod->exists()) {
                                     <tr>
                                         <td style="width: 20%;">Počáteční místo</td>
                                         <td>
-                                            <span>Bochum</span>
+                                            <span class="d-block"> {{ origin }}</span>
+                                            <span class="d-block" v-if="origin_uncertain">
+                                                <small>(Nejisté počáteční místo)</small>
+                                            </span>
+                                            <span class="d-block" v-if="origin_inferred">
+                                                <small>(Počáteční místo vyvozeno z obsahu dopisu)</small>
+                                            </span>
                                         </td>
                                     </tr>
                                     <tr>
-                                        <td>Místo určení</td>
+                                        <td>Počáteční místo uvedené v dopise</td>
                                         <td>
-                                            <span>Oslo</span>
+                                            <span> {{ origin_marked }}</span>
+                                        </td>
+                                    </tr>
+
+                                    <tr>
+                                        <td style="width: 20%;">Místo určení</td>
+                                        <td>
+                                            <span class="d-block"> {{ destination }}</span>
+                                            <span class="d-block" v-if="dest_uncertain">
+                                                <small>(Nejisté místo určení)</small>
+                                            </span>
+                                            <span class="d-block" v-if="dest_inferred">
+                                                <small>(Místo určení vyvozeno z obsahu dopisu)</small>
+                                            </span>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>Místo určení uvedené v dopise</td>
+                                        <td>
+                                            <span> {{ dest_marked }}</span>
                                         </td>
                                     </tr>
                                 </tbody>
@@ -98,28 +180,71 @@ if (!$pod->exists()) {
                                 <tbody>
                                     <tr>
                                         <td style="width: 20%;">Jazyk</td>
-                                        <td><span>german</span>
+                                        <td>
+                                            <span v-for="lang in languages" class="d-block"> {{ lang }}</span>
                                         </td>
                                     </tr>
 
                                     <tr>
+                                        <td>Klíčová slova</td>
+                                        <td>
+                                            <span v-for="keyword in keywords" class="d-block"> {{ keyword }}</span>
+                                        </td>
+                                    </tr>
+
+                                    <tr>
+                                        <td>Abstrakt</td>
+                                        <td>{{ abstract }}</td>
+                                    </tr>
+
+                                    <tr>
                                         <td>Incipit</td>
-                                        <td>Herzlichen Dank für Ihren ausführlichen Brief</td>
+                                        <td v-html="incipit"></td>
+                                    </tr>
+
+                                    <tr>
+                                        <td>Explicit</td>
+                                        <td v-html="explicit"></td>
                                     </tr>
 
                                     <tr>
                                         <td>Zmíněné osoby</td>
                                         <td>
-                                            <ul class="list-unstyled">
-                                                <li>Comenius</li>
-                                            </ul>
+                                            <span v-for="m in mentioned" class="d-block"> {{ m }}</span>
                                         </td>
                                     </tr>
 
+                                    <tr v-if="people_mentioned_notes">
+                                        <td>Poznámky o zmíněných osobách</td>
+                                        <td>{{ people_mentioned_notes }}</td>
+                                    </tr>
+
+                                    <tr v-if="notes_public">
+                                        <td>Poznámky k dopisu</td>
+                                        <td>{{ notes_public }}</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                        <div v-if="rel_rec_name" class="mb-5">
+                            <h5>Související zdroje</h5>
+                            <table class="table">
+                                <tbody>
+                                    <tr>
+                                        <td>
+                                            <a :href="rel_rec_url" target="_blank">{{ rel_rec_name }}</a>
+                                        </td>
+                                    </tr>
                                 </tbody>
                             </table>
                         </div>
                     </div>
+                </div>
+
+                <div class="col-md-3">
+                    <a href="<?= $link_letter_edit ?>">Upravit dopis</a>
+                    <br>
+                    <a href="<?= $link_dashboard ?>">Přehled dopisů</a>
                 </div>
             </div>
         </div>
