93fa0c972c1e0847c30bc1092762152d28b58aab
Jarka <jarka.p@gmail.com>
2019-02-09 16:31:04 +0100

    edit 404 template

--- 404.php
+++ 404.php
@@ -1,7 +1,20 @@
-<?php get_header(); ?>
+<?php
 
-<div class="col">
-    <h1>Stránka nebyla nalezena</h1>
+get_header();
+
+require 'partials/general-nav.php';
+
+?>
+
+<div class="container mt-5">
+    <div class="row">
+
+        <div class="col-md">
+            <h1>Stránka nebyla nalezena.</h1>
+        </div>
+
+    </div>
 </div>
+
 
 <?php get_footer();
