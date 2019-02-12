0591af69b999383aac415e8d5117b95c93b5fe66
JarkaP <jarka.p@gmail.com>
2019-02-12 11:48:10 +0100

    add fields to export

--- ajax/export.php
+++ ajax/export.php
@@ -37,18 +37,60 @@ function export_letters()
     $spreadsheet->setActiveSheetIndex(0)
          ->setCellValue('A1', 'Number')
          ->setCellValue('B1', 'Name')
-         ->setCellValue('C1', 'Date marked');
+         ->setCellValue('C1', 'Date marked')
+         ->setCellValue('D1', 'Author')
+         ->setCellValue('E1', 'Recipient')
+         ->setCellValue('F1', 'Origin')
+         ->setCellValue('G1', 'Destination');
 
     $results = [];
     $index = 0;
     $col = 2;
     while ($pod->fetch()) {
+        $authors = '';
+        $recipients = '';
+        $origins = '';
+        $destinations = '';
+
+        $authors_full = $pod->field('l_author');
+        $recipients_full = $pod->field('recipient');
+        $origins_full = $pod->field('origin');
+        $destinations_full = $pod->field('dest');
+
+        if (!empty($authors_full)) {
+            foreach ($authors_full as $rel_author) {
+                $authors .= $rel_author['name'] . ';';
+            }
+        }
+
+        if (!empty($recipients_full)) {
+            foreach ($recipients_full as $rel_recipient) {
+                $recipients[] = $rel_recipient['name'] . ';';
+            }
+        }
+
+        if (!empty($origins_full)) {
+            foreach ($origins_full as $o) {
+                $origins[] = $o['name'] . ';';
+            }
+        }
+
+        if (!empty($destinations_full)) {
+            foreach ($destinations_full as $d) {
+                $destinations[] = $d['name'] . ';';
+            }
+        }
+
         $spreadsheet
         ->setActiveSheetIndex(0)
         ->setCellValueByColumnAndRow(1, $col, $pod->field('l_number'))
         ->setCellValueByColumnAndRow(2, $col, $pod->field('name'))
-        ->setCellValueByColumnAndRow(3, $col, $pod->field('date_marked'));
-    
+        ->setCellValueByColumnAndRow(3, $col, $pod->field('date_marked'))
+        ->setCellValueByColumnAndRow(4, $col, $authors)
+        ->setCellValueByColumnAndRow(5, $col, $recipients)
+        ->setCellValueByColumnAndRow(6, $col, $origins)
+        ->setCellValueByColumnAndRow(7, $col, $destinations);
+        
         $col++;
         $index++;
     }
