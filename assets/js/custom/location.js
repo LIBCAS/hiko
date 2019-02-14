b0307bac328fcb7260ad9b0d398e2fa86c465257
JarkaP <jarka.p@gmail.com>
2019-02-14 11:45:27 +0100

    addNewLocationItem()

--- assets/js/custom/location.js
+++ assets/js/custom/location.js
@@ -14,3 +14,58 @@ if (document.getElementById('repository')) {
         },
     })
 }
+
+function addNewLocationItem(type, title) {
+    Swal.fire({
+        title: title,
+        type: 'question',
+        input: 'text',
+        inputValidator: value => {
+            if (value.length < 3) {
+                return 'Zadejte hodnotu'
+            }
+        },
+        buttonsStyling: false,
+        showCancelButton: true,
+        confirmButtonText: 'Uložit',
+        cancelButtonText: 'Zrušit',
+        confirmButtonClass: 'btn btn-primary btn-lg mr-1',
+        cancelButtonClass: 'btn btn-secondary btn-lg ml-1',
+        showLoaderOnConfirm: true,
+        preConfirm: function(value) {
+            return axios
+                .post(
+                    ajaxUrl + '?action=add_new_location_data',
+                    {
+                        ['type']: type,
+                        ['item']: value,
+                    },
+                    {
+                        headers: {
+                            'Content-Type': 'application/json;charset=utf-8',
+                        },
+                    }
+                )
+                .then(function(response) {
+                    console.log(response.data)
+                    return response.data
+                })
+                .catch(function(error) {
+                    Swal.showValidationMessage(
+                        `Při ukládání došlo k chybě: ${error}`
+                    )
+                })
+        },
+        allowOutsideClick: () => !Swal.isLoading(),
+    }).then(result => {
+        if (result.value) {
+            Swal.fire({
+                title: 'Položka byla úspěšně přidána',
+                type: 'success',
+                buttonsStyling: false,
+                confirmButtonText: 'OK',
+                confirmButtonClass: 'btn btn-primary btn-lg',
+            })
+        }
+    })
+}
