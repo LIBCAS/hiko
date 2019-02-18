9bb6d1ce86248490d4d7c1043f445c801f431210
JarkaP <jarka.p@gmail.com>
2019-02-18 17:20:54 +0100

    create full export

--- ajax/export.php
+++ ajax/export.php
@@ -2,106 +2,146 @@
 
 require_once get_template_directory() . '/vendor/autoload.php';
 
-use PhpOffice\PhpSpreadsheet\Spreadsheet;
-use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+
+use Box\Spout\Writer\WriterFactory;
+use Box\Spout\Common\Type;
 
 function export_letters()
 {
 
-    if (!array_key_exists('l_type', $_GET)) {
+    if (!array_key_exists('type', $_GET)) {
         wp_send_json_error('Not found', 404);
     }
 
-    $type = sanitize_text_field($_GET['l_type']);
+    $type = sanitize_text_field($_GET['type']);
+    $types = get_hiko_post_types($type);
 
-    if ($type == 'bl_letter' && has_user_permission('blekastad_editor')) {
-        $filename = 'blekastad';
-    } else {
+    if (!has_user_permission($types['editor'])) {
         wp_send_json_error('Not allowed', 403);
     }
 
     $pod = pods(
-        $type,
+        $types['letter'],
         [
         'orderby'=> 't.created DESC',
         'limit' => -1
         ]
     );
 
-    $spreadsheet = new Spreadsheet();
-    $spreadsheet
-    ->getProperties()
-    ->setCreator('HIKO')
-    ->setTitle('Korespondence MB');
+    $writer = WriterFactory::create(Type::XLSX);
+    $writer->openToBrowser($type . '-export.xlsx');
 
-    $spreadsheet->setActiveSheetIndex(0)
-         ->setCellValue('A1', 'Number')
-         ->setCellValue('B1', 'Name')
-         ->setCellValue('C1', 'Date marked')
-         ->setCellValue('D1', 'Author')
-         ->setCellValue('E1', 'Recipient')
-         ->setCellValue('F1', 'Origin')
-         ->setCellValue('G1', 'Destination');
-
-    $results = [];
-    $index = 0;
-    $col = 2;
+    $writer->addRow([
+            'l_number',
+            'date_year',
+            'date_month',
+            'date_day',
+            'date_marked',
+            'range_year',
+            'range_month',
+            'range_day',
+            'date_uncertain',
+            'date_approximate',
+            'date_is_range',
+            'date_note',
+            'author',
+            'author_marked',
+            'author_uncertain',
+            'author_inferred',
+            'author_note',
+            'recipient',
+            'recipient_marked',
+            'recipient_inferred',
+            'recipient_uncertain',
+            'recipient_notes',
+            'origin',
+            'origin_marked',
+            'origin_inferred',
+            'origin_uncertain',
+            'origin_note',
+            'destination',
+            'destination_marked',
+            'destination_uncertain',
+            'destination_inferred',
+            'destination_note',
+            'languages',
+            'keywords',
+            'abstract',
+            'incipit',
+            'explicit',
+            'people_mentioned',
+            'people_mentioned_notes',
+            'notes_public',
+            'rel_rec_name',
+            'rel_rec_url',
+            'ms_manifestation',
+            'repository',
+            'archive',
+            'collection',
+            'signature',
+            'description',
+            'status',
+        ]);
     while ($pod->fetch()) {
         $authors = '';
         $recipients = '';
         $origins = '';
         $destinations = '';
-
-        $authors_full = $pod->field('l_author');
-        $recipients_full = $pod->field('recipient');
-        $origins_full = $pod->field('origin');
-        $destinations_full = $pod->field('dest');
-
-        if (!empty($authors_full)) {
-            foreach ($authors_full as $rel_author) {
-                $authors .= $rel_author['name'] . ';';
-            }
-        }
-
-        if (!empty($recipients_full)) {
-            foreach ($recipients_full as $rel_recipient) {
-                $recipients[] = $rel_recipient['name'] . ';';
-            }
-        }
-
-        if (!empty($origins_full)) {
-            foreach ($origins_full as $o) {
-                $origins[] = $o['name'] . ';';
-            }
-        }
-
-        if (!empty($destinations_full)) {
-            foreach ($destinations_full as $d) {
-                $destinations[] = $d['name'] . ';';
-            }
-        }
-
-        $spreadsheet
-        ->setActiveSheetIndex(0)
-        ->setCellValueByColumnAndRow(1, $col, $pod->field('l_number'))
-        ->setCellValueByColumnAndRow(2, $col, $pod->field('name'))
-        ->setCellValueByColumnAndRow(3, $col, $pod->field('date_marked'))
-        ->setCellValueByColumnAndRow(4, $col, $authors)
-        ->setCellValueByColumnAndRow(5, $col, $recipients)
-        ->setCellValueByColumnAndRow(6, $col, $origins)
-        ->setCellValueByColumnAndRow(7, $col, $destinations);
-        
-        $col++;
-        $index++;
+        $people_mentioned = '';
+        $row = [
+            (string) $pod->field('l_number'),
+            (string) $pod->field('date_year'),
+            (string) $pod->field('date_month'),
+            (string) $pod->field('date_day'),
+            (string) $pod->field('date_marked'),
+            (string) $pod->field('range_year'),
+            (string) $pod->field('range_month'),
+            (string) $pod->field('range_day'),
+            (string) ($pod->field('date_uncertain') == '1') ? '1' : '',
+            (string) ($pod->field('date_approximate') == '1') ? '1' : '',
+            (string) ($pod->field('date_is_range') == '1') ? '1' : '',
+            (string) $pod->field('date_note'),
+            (string) $authors,
+            (string) $pod->field('l_author_marked'),
+            (string) ($pod->field('author_uncertain') == '1') ? '1' : '',
+            (string) ($pod->field('author_inferred') == '1') ? '1' : '',
+            (string) $pod->field('author_note'),
+            (string) $recipients,
+            (string) $pod->field('recipient_marked'),
+            (string) ($pod->field('recipient_inferred') == '1') ? '1' : '',
+            (string) ($pod->field('recipient_uncertain') == '1') ? '1' : '',
+            (string) $pod->field('recipient_notes'),
+            (string) $origins,
+            (string) $pod->field('origin_marked'),
+            (string) ($pod->field('origin_inferred') == '1') ? '1' : '',
+            (string) ($pod->field('origin_uncertain') == '1') ? '1' : '',
+            (string) $pod->field('origin_note'),
+            (string) $destinations,
+            (string) $pod->field('dest_marked'),
+            (string) ($pod->field('dest_uncertain') == '1') ? '1' : '',
+            (string) ($pod->field('dest_inferred') == '1') ? '1' : '',
+            (string) $pod->field('dest_note'),
+            (string) $pod->field('languages'),
+            (string) $pod->field('keywords'),
+            (string) $pod->field('abstract'),
+            (string) $pod->field('incipit'),
+            (string) $pod->field('explicit'),
+            (string) $people_mentioned,
+            (string) $pod->field('people_mentioned_notes'),
+            (string) $pod->field('notes_public'),
+            (string) $pod->field('rel_rec_name'),
+            (string) $pod->field('rel_rec_url'),
+            (string) (is_string($pod->field('ms_manifestation'))) ? $pod->field('ms_manifestation') : '',//($pod->field('ms_manifestation') === null) ? '' : $pod->field('ms_manifestation'),
+            (string) $pod->field('repository'),
+            (string) $pod->field('archive'),
+            (string) $pod->field('collection'),
+            (string) $pod->field('signature'),
+            (string) $pod->field('name'),
+            (string) (is_string($pod->field('status'))) ? $pod->field('status') : '',
+        ];
+        $writer->addRow($row);
     }
-
-    $writer = new \PhpOffice\PhpSpreadsheet\Writer\Xlsx($spreadsheet);
-
-    header('Content-Type: Application/vnd.ms-excel');
-    header('Content-Disposition: attachment; filename="' . $filename . '-export.xlsx"');
-    header('Content-Description: File Transfer');
-    header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
-    $writer->save('php://output');
+    $writer->close();
     wp_die();
 }
 add_action('wp_ajax_export_letters', 'export_letters');
