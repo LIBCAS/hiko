316e1c3f194bd6dc2d277b439325a5546e267328
JarkaP <jarka.p@gmail.com>
2019-04-17 17:33:25 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,3 +1,10 @@
+    <div class="footer container mt-5 py-3">
+        <div class="row justify-content-center">
+            <div class="col">
+                <a href="mailto:pachlova@lib.cas.cz?cc=knavcr@lib.cas.cz&subject=hiko%20administrace" class="d-none">Něco se pokazilo</a>
+            </div>
+        </div>
+    </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap.native@2.0.15/dist/bootstrap-native-v4.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
