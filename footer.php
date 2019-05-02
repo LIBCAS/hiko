386729b04548619a191c1bfb1925bd9ba133eb02
JarkaP <jarka.p@gmail.com>
2019-05-02 14:30:19 +0200

    update dependencies and load them only on some pages

--- footer.php
+++ footer.php
@@ -7,14 +7,25 @@
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap.native@2.0.15/dist/bootstrap-native-v4.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
-    <script src="https://unpkg.com/axios@0.18.0/dist/axios.min.js"></script>
+
+
+    <?php if (current_user_can('administrator')) : ?>
+        <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+    <?php else : ?>
+        <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.min.js"></script>
+    <?php endif; ?>
+
+    <?php if (is_page_template('page-templates/page-images.php')) : ?>
+        <script src="https://cdn.jsdelivr.net/npm/uppy@1.0.0/dist/uppy.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/sortablejs@1.9.0/Sortable.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/vuedraggable@2.20.0/dist/vuedraggable.umd.min.js"></script>
+    <?php endif; ?>
+
+    <script src="https://cdn.jsdelivr.net/npm/axios@0.18.0/dist/axios.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/vue-tables-2@1.4.70/dist/vue-tables-2.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@8"></script>
-    <script src="https://transloadit.edgly.net/releases/uppy/v0.29.1/dist/uppy.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/sortablejs@latest/Sortable.min.js"></script>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/Vue.Draggable/2.17.0/vuedraggable.min.js"></script>
-    <script src="https://unpkg.com/vue-multiselect@2.1.0"></script>
+    <script src="https://cdn.jsdelivr.net/npm/vue-multiselect@2.1.6/dist/vue-multiselect.min.js"></script>
+
     <script src="<?= get_template_directory_uri(); ?>/assets/dist/custom.min.js?v=<?= filemtime(get_template_directory() . '/assets/dist/custom.min.js'); ?>"></script>
 
     <?php wp_footer(); ?>
