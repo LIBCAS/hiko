dc4d477a18fce6c08b8df2d4b231e3788c8a9daf
JarkaP <jarka.p@gmail.com>
2019-10-10 15:02:36 +0200

    update front page

--- front-page.php
+++ front-page.php
@@ -4,62 +4,37 @@ get_header();
 
 require 'partials/general-nav.php';
 
+$cardsData = [
+    'Korespondence Milady Blekastad' => 'blekastad',
+    'Korespondence TGM' => 'tgm',
+    'Zkušební DB' => 'demo',
+];
+
 ?>
 
 <div class="container mt-5">
     <div class="row" style="min-height: 75vh;">
-
-        <div class="col-md">
-            <div class="card mb-3 mw-400">
-                <h3 class="card-header">Korespondence Milady Blekastad</h3>
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item">
-                        <a href="<?= home_url('/blekastad/letters/') ?>" class="card-link">Dopisy</a>
-                    </li>
-                    <li class="list-group-item">
-                        <a href="<?= home_url('/blekastad/persons/') ?>" class="card-link">Lidé</a>
-                    </li>
-                    <li class="list-group-item">
-                        <a href="<?= home_url('/blekastad/places/') ?>" class="card-link">Místa</a>
-                    </li>
-                </ul>
+        <?php foreach ($cardsData as $key => $value) : ?>
+            <div class="col-md">
+                <div class="card mb-3 mw-400">
+                    <h3 class="card-header"><?= $key; ?></h3>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">
+                            <a href="<?= home_url("/$value/letters/"); ?>" class="card-link">Dopisy</a>
+                        </li>
+                        <li class="list-group-item">
+                            <a href="<?= home_url("/$value/persons/"); ?>" class="card-link">Lidé</a>
+                        </li>
+                        <li class="list-group-item">
+                            <a href="<?= home_url("/$value/places/"); ?>" class="card-link">Místa</a>
+                        </li>
+                        <li class="list-group-item">
+                            <a href="<?= home_url("/$value/keywords/"); ?>" class="card-link">Klíčová slova</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
-        </div>
-
-        <div class="col-md">
-            <div class="card mb-3 mw-400">
-                <h3 class="card-header">Korespondence TGM</h3>
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item">
-                        <a href="<?= home_url('/tgm/letters/') ?>" class="card-link">Dopisy</a>
-                    </li>
-                    <li class="list-group-item">
-                        <a href="<?= home_url('/tgm/persons/') ?>" class="card-link">Lidé</a>
-                    </li>
-                    <li class="list-group-item">
-                        <a href="<?= home_url('/tgm/places/') ?>" class="card-link">Místa</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-
-        <div class="col-md">
-            <div class="card mb-3 mw-400">
-                <h3 class="card-header">Zkušební DB</h3>
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item">
-                        <a href="<?= home_url('/demo/letters/') ?>" class="card-link">Dopisy</a>
-                    </li>
-                    <li class="list-group-item">
-                        <a href="<?= home_url('/demo/persons/') ?>" class="card-link">Lidé</a>
-                    </li>
-                    <li class="list-group-item">
-                        <a href="<?= home_url('/demo/places/') ?>" class="card-link">Místa</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-
+        <?php endforeach; ?>
     </div>
 </div>
 
