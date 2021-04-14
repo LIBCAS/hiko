fb4cf084b3e5a33408aa55195d6e0edf29ff1e6d
Jarka Pachlová <jarka.p@gmail.com>
2021-04-14 18:18:10 +0200

    remove bootstrap js

--- footer.php
+++ footer.php
@@ -6,7 +6,7 @@
         </div>
     </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap.native@2.0.15/dist/bootstrap-native-v4.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js"></script>
 
     <?php if (is_page_template('page-templates/page-images.php')) : ?>
         <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.min.js"></script>
@@ -14,8 +14,6 @@
         <script src="https://cdn.jsdelivr.net/npm/sortablejs@1.9.0/Sortable.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/vuedraggable@2.20.0/dist/vuedraggable.umd.min.js"></script>
     <?php endif; ?>
-
-    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/axios@0.18.0/dist/axios.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/tabulator-tables@4.9.3/dist/js/tabulator.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@8"></script>
--- partials/general-nav.php
+++ partials/general-nav.php
@@ -13,18 +13,18 @@ $navData = [
 ?>
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container">
+    <div class="container" x-data="{ openedMenu: false, openedDD: false }">
         <a class="navbar-brand" href="<?= home_url() ?>">HIKO</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button @click="openedMenu = !openedMenu" class="navbar-toggler" type="button" aria-controls="navbarSupportedContent" x-bind:aria-expanded="openedMenu" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <div class="collapse navbar-collapse" id="navbarSupportedContent" :class="{ 'show': openedMenu }">
             <ul class="navbar-nav">
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="letters-dd" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <li class="nav-item dropdown" :class="{ 'show': openedDD }">
+                    <a @click="openedDD = !openedDD" class="nav-link dropdown-toggle" href="#" id="letters-dd" role="button" aria-haspopup="true" x-bind:aria-expanded="openedDD">
                         Korespondence
                     </a>
-                    <div class="dropdown-menu" aria-labelledby="letters-dd">
+                    <div x-show="openedDD" class="dropdown-menu" :class="{ 'show': openedDD }" aria-labelledby="letters-dd">
                         <?php foreach ($navData as $key => $value) : ?>
                             <a class="dropdown-item" href="<?= home_url('#' . $value) ?>">
                                 <?= $key; ?>
