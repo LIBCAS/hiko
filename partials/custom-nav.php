3b33f67b7f54a35f04ad9468901892e1bb337846
Jarka Pachlová <jarka.p@gmail.com>
2021-05-20 17:51:30 +0200

    fix nav

--- partials/custom-nav.php
+++ partials/custom-nav.php
@@ -4,14 +4,13 @@ $path = $pods_types['path'];
 $title = $pods_types['title'];
 ?>
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav x-data="{ openedMenu: false, openedDD: false }" class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container">
         <a class="navbar-brand" href="<?= home_url($path) ?>"><?= $title ?></a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button @click="openedMenu = !openedMenu" class="navbar-toggler" type="button" aria-controls="navbarSupportedContent" x-bind:aria-expanded="openedMenu" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <div class="collapse navbar-collapse" id="navbarSupportedContent" :class="{ 'show': openedMenu }">
             <ul class="navbar-nav">
                 <li class="nav-item">
                     <a class="nav-link" href="<?= home_url($path . '/letters/') ?>">
