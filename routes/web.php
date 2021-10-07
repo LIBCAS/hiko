7300aa6cbb8d163a9fc6496f09d941d7bd42dde7
Jarka Pachlová <jarka.p@gmail.com>
2021-10-08 00:20:07 +0200

    prefix routes

--- routes/web.php
+++ routes/web.php
@@ -23,79 +23,82 @@ Route::get('/dashboard', DashboardController::class)
     ->name('dashboard')
     ->middleware('auth');
 
-// users
-Route::get('/users', [UserController::class, 'index'])
-    ->name('users')
-    ->middleware(['auth', 'can:manage-users']);
+Route::prefix('users')->group(function () {
+    Route::get('/', [UserController::class, 'index'])
+        ->name('users')
+        ->middleware(['auth', 'can:manage-users']);
 
-Route::get('/users/create', [UserController::class, 'create'])
-    ->name('users.create')
-    ->middleware(['auth', 'can:manage-users']);
+    Route::get('/create', [UserController::class, 'create'])
+        ->name('users.create')
+        ->middleware(['auth', 'can:manage-users']);
 
-Route::get('/users/{user}/edit', [UserController::class, 'edit'])
-    ->name('users.edit')
-    ->middleware(['auth', 'can:manage-users']);
+    Route::get('/{user}/edit', [UserController::class, 'edit'])
+        ->name('users.edit')
+        ->middleware(['auth', 'can:manage-users']);
 
-Route::post('/users', [UserController::class, 'store'])
-    ->name('users.store')
-    ->middleware(['auth', 'can:manage-users']);
+    Route::post('/', [UserController::class, 'store'])
+        ->name('users.store')
+        ->middleware(['auth', 'can:manage-users']);
 
-Route::put('/users/{user}', [UserController::class, 'update'])
-    ->name('users.update')
-    ->middleware(['auth', 'can:manage-users']);
+    Route::put('/{user}', [UserController::class, 'update'])
+        ->name('users.update')
+        ->middleware(['auth', 'can:manage-users']);
 
-Route::delete('/users/{user}', [UserController::class, 'destroy'])
-    ->name('users.destroy')
-    ->middleware(['auth', 'can:manage-users']);
+    Route::delete('/{user}', [UserController::class, 'destroy'])
+        ->name('users.destroy')
+        ->middleware(['auth', 'can:manage-users']);
+});
 
-// locations
-Route::get('/locations', [LocationController::class, 'index'])
-    ->name('locations')
-    ->middleware(['auth', 'can:manage-metadata']);
+Route::prefix('locations')->group(function () {
+    Route::get('/', [LocationController::class, 'index'])
+        ->name('locations')
+        ->middleware(['auth', 'can:manage-metadata']);
 
-Route::get('/locations/create', [LocationController::class, 'create'])
-    ->name('locations.create')
-    ->middleware(['auth', 'can:manage-metadata']);
+    Route::get('/create', [LocationController::class, 'create'])
+        ->name('locations.create')
+        ->middleware(['auth', 'can:manage-metadata']);
 
-Route::get('/locations/{location}/edit', [LocationController::class, 'edit'])
-    ->name('locations.edit')
-    ->middleware(['auth', 'can:manage-metadata']);
+    Route::get('/{location}/edit', [LocationController::class, 'edit'])
+        ->name('locations.edit')
+        ->middleware(['auth', 'can:manage-metadata']);
 
-Route::post('/locations', [LocationController::class, 'store'])
-    ->name('locations.store')
-    ->middleware(['auth', 'can:manage-metadata']);
+    Route::post('/', [LocationController::class, 'store'])
+        ->name('locations.store')
+        ->middleware(['auth', 'can:manage-metadata']);
 
-Route::put('/locations/{location}', [LocationController::class, 'update'])
-    ->name('locations.update')
-    ->middleware(['auth', 'can:manage-metadata']);
+    Route::put('/{location}', [LocationController::class, 'update'])
+        ->name('locations.update')
+        ->middleware(['auth', 'can:manage-metadata']);
 
-Route::delete('/locations/{location}', [LocationController::class, 'destroy'])
-    ->name('locations.destroy')
-    ->middleware(['auth', 'can:manage-metadata']);
+    Route::delete('/{location}', [LocationController::class, 'destroy'])
+        ->name('locations.destroy')
+        ->middleware(['auth', 'can:manage-metadata']);
+});
 
-// professions
-Route::get('/professions', [ProfessionController::class, 'index'])
-    ->name('professions')
-    ->middleware(['auth', 'can:manage-metadata']);
+Route::prefix('professions')->group(function () {
+    Route::get('/', [ProfessionController::class, 'index'])
+        ->name('professions')
+        ->middleware(['auth', 'can:manage-metadata']);
 
-Route::get('/professions/create', [ProfessionController::class, 'create'])
-    ->name('professions.create')
-    ->middleware(['auth', 'can:manage-metadata']);
+    Route::get('/create', [ProfessionController::class, 'create'])
+        ->name('professions.create')
+        ->middleware(['auth', 'can:manage-metadata']);
 
-Route::get('/professions/{profession}/edit', [ProfessionController::class, 'edit'])
-    ->name('professions.edit')
-    ->middleware(['auth', 'can:manage-metadata']);
+    Route::get('/{profession}/edit', [ProfessionController::class, 'edit'])
+        ->name('professions.edit')
+        ->middleware(['auth', 'can:manage-metadata']);
 
-Route::post('/professions', [ProfessionController::class, 'store'])
-    ->name('professions.store')
-    ->middleware(['auth', 'can:manage-metadata']);
+    Route::post('/', [ProfessionController::class, 'store'])
+        ->name('professions.store')
+        ->middleware(['auth', 'can:manage-metadata']);
 
-Route::put('/professions/{profession}', [ProfessionController::class, 'update'])
-    ->name('professions.update')
-    ->middleware(['auth', 'can:manage-metadata']);
+    Route::put('/{profession}', [ProfessionController::class, 'update'])
+        ->name('professions.update')
+        ->middleware(['auth', 'can:manage-metadata']);
 
-Route::delete('/professions/{profession}', [ProfessionController::class, 'destroy'])
-    ->name('professions.destroy')
-    ->middleware(['auth', 'can:manage-metadata']);
+    Route::delete('/{profession}', [ProfessionController::class, 'destroy'])
+        ->name('professions.destroy')
+        ->middleware(['auth', 'can:manage-metadata']);
+});
 
 require __DIR__ . '/auth.php';
