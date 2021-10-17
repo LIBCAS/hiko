83f78286732de1aa9c9d29395417af3237338eb8
Jarka Pachlová <jarka.p@gmail.com>
2021-10-18 00:44:10 +0200

    resize textarea by content

--- resources/views/components/textarea.blade.php
+++ resources/views/components/textarea.blade.php
@@ -1,3 +1,5 @@
 @props(['disabled' => false])
 
-<textarea {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'rounded-md shadow-sm border-gray-300 focus:border-primary']) !!}>{{ $slot }}</textarea>
+<textarea x-data="{ resize: () => { $el.style.height = '120px'; $el.style.height = $el.scrollHeight + 'px' } }"
+    x-init="resize()" @input="resize()" {{ $disabled ? 'disabled' : '' }}
+    {!! $attributes->merge(['class' => 'rounded-md shadow-sm border-gray-300 focus:border-primary']) !!}>{{ $slot }}</textarea>
