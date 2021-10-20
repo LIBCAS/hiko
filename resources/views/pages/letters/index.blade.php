d4d945f5474f0713f718219d9bd97b731fbcb4dd
Jarka Pachlová <jarka.p@gmail.com>
2021-10-21 00:22:04 +0200

    edit styles

--- resources/views/pages/letters/index.blade.php
+++ resources/views/pages/letters/index.blade.php
@@ -4,4 +4,13 @@
         <x-create-link label="{{ __('Nový dopis') }}" link="{{ route('letters.create') }}" />
     @endcan
     <livewire:letters-table />
+    @push('styles')
+        <style>
+            .table input,
+            .table select,
+            .table .table-cell {
+                max-width: 140px;
+            }
+        </style>
+    @endpush
 </x-app-layout>
