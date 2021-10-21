b18945de969223b6212d88650c62282afeda2164
Jarka Pachlová <jarka.p@gmail.com>
2021-10-21 12:49:22 +0200

    fix favicon

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -7,7 +7,7 @@
         <title>{{ config('app.name') }}</title>
         <link rel="stylesheet" href="{{ asset('dist/app.css') }}">
         <script src="{{ asset('dist/app.js') }}" defer></script>
-        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>✉️</text></svg>">
+        <link rel="icon" type="image/svg+xml" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 width=%22256%22 height=%22256%22 viewBox=%220 0 100 100%22><text x=%2250%%22 y=%2250%%22 dominant-baseline=%22central%22 text-anchor=%22middle%22 font-size=%2290%22>✉️</text></svg>" />
     </head>
     <body class="px-6 font-sans antialiased text-gray-900 bg-gray-100">
         {{ $slot }}
