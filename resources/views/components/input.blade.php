7031d3a153a63e1a6a2d4e18fe4a779d7c1f34d1
JarkaP <jarka.p@gmail.com>
2021-11-02 17:09:42 +0100

    update focus state

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -1,3 +1,3 @@
 @props(['disabled' => false])
 
-<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'rounded-md shadow-sm border-gray-300 focus:border-primary']) !!}>
+<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'rounded-md shadow-sm border-gray-300']) !!}>
