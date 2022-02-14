c65411f174b12c2826dcd4795bc323c2dd57104b
Jarka Pachlová <jarka.p@gmail.com>
2022-02-14 23:52:13 +0100

    add translations

--- resources/views/pages/professions/index.blade.php
+++ resources/views/pages/professions/index.blade.php
@@ -1,22 +1,22 @@
 <x-app-layout :title="$title">
     <x-success-alert />
     @can('manage-metadata')
-        <x-create-link label="{{ __('Nová profese') }}" link="{{ route('professions.create') }}" />
+        <x-create-link label="{{ __('hiko.new_profession') }}" link="{{ route('professions.create') }}" />
         <a href="{{ route('professions.export') }}" class="inline-block mt-3 text-sm font-semibold">
-            {{ __('Exportovat') }}
+            {{ __('hiko.export') }}
         </a>
     @endcan
     <livewire:professions-table />
     @can('manage-metadata')
-        <x-create-link label="{{ __('Nová kategorie profese') }}" link="{{ route('professions.category.create') }}"
+        <x-create-link label="{{ __('hiko.new_professions_category') }}" link="{{ route('professions.category.create') }}"
             class="mt-16" />
         <a href="{{ route('professions.category.export') }}" class="inline-block mt-3 text-sm font-semibold">
-            {{ __('Exportovat') }}
+            {{ __('hiko.export') }}
         </a>
     @endcan
     @cannot('manage-metadata')
         <p class="mt-16 font-bold">
-            {{ __('Kategorie profese') }}
+            {{ __('hiko.professions_category') }}
         </p>
     @endcannot
     <livewire:profession-category-table />
