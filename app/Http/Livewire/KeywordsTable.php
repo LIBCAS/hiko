2ae851061cc1b8795cb3948ae43d0fe9d52e26eb
JarkaP <jarka.p@gmail.com>
2022-02-15 16:42:12 +0100

    update keywords table

--- app/Http/Livewire/KeywordsTable.php
+++ app/Http/Livewire/KeywordsTable.php
@@ -3,49 +3,84 @@
 namespace App\Http\Livewire;
 
 use App\Models\Keyword;
-use Mediconesystems\LivewireDatatables\Column;
-use Mediconesystems\LivewireDatatables\Http\Livewire\LivewireDatatable;
+use Livewire\Component;
+use Livewire\WithPagination;
+use Illuminate\Support\Facades\DB;
 
-class KeywordsTable extends LivewireDatatable
+class KeywordsTable extends Component
 {
-    public $model = Keyword::class;
+    use WithPagination;
 
-    public function columns()
+    public $filters = [
+        'order' => 'cs',
+    ];
+
+    public function search()
+    {
+        $this->resetPage('keywordsPage');
+    }
+
+    public function render()
+    {
+        $keywords = $this->findKeywords();
+
+        return view('livewire.keywords-table', [
+            'tableData' => $this->formatTableData($keywords),
+            'pagination' => $keywords,
+        ]);
+    }
+
+    protected function findKeywords()
+    {
+        $query = Keyword::select('id', 'keyword_category_id', 'name', DB::raw("LOWER(JSON_EXTRACT(name, '$.cs')) AS cs"), DB::raw("LOWER(JSON_EXTRACT(name, '$.en')) AS en"))
+            ->with([
+                'keyword_category' => function ($subquery) {
+                    $subquery->select('id', 'name');
+                },
+            ]);
+
+        if (isset($this->filters['cs']) && !empty($this->filters['cs'])) {
+            $query->whereRaw("LOWER(JSON_EXTRACT(name, '$.cs')) like ?", ["%{$this->filters['cs']}%"]);
+        }
+
+        if (isset($this->filters['en']) && !empty($this->filters['en'])) {
+            $query->whereRaw("LOWER(JSON_EXTRACT(name, '$.en')) like ?", ["%{$this->filters['en']}%"]);
+        }
+
+        if (isset($this->filters['category']) && !empty($this->filters['category'])) {
+            $query->whereHas('keyword_category', function ($subquery) {
+                $subquery
+                    ->whereRaw("LOWER(JSON_EXTRACT(name, '$.en')) like ?", ["%{$this->filters['category']}%"])
+                    ->orWhereRaw("LOWER(JSON_EXTRACT(name, '$.cs')) like ?", ["%{$this->filters['category']}%"]);
+            });
+        }
+
+        $query->orderBy($this->filters['order']);
+
+        return $query->paginate(10, ['*'], 'keywordsPage');
+    }
+
+    protected function formatTableData($data)
     {
         return [
-            Column::callback(['id'], function ($id) {
-                return view('tables.edit-link', ['route' => route('keywords.edit', $id), 'label' => __('Upravit')]);
-            }),
-
-            Column::callback(['name->en'], function ($name) {
-                if (empty($name) || $name === 'null') {
-                    return '';
-                }
-                return $name;
-            }, 'en')
-                ->defaultSort('asc')
-                ->filterable()
-                ->filterOn("JSON_EXTRACT(keywords.name, '$.en')")
-                ->label('en'),
-
-            Column::callback(['name->cs'], function ($name) {
-                if (empty($name) || $name === 'null') {
-                    return '';
-                }
-                return $name;
-            }, 'cs')
-                ->filterable()
-                ->filterOn("JSON_EXTRACT(keywords.name, '$.cs')")
-                ->label('cs'),
-
-            Column::callback(['keyword_category.name'], function ($keyword_category) {
-                if (empty($keyword_category)) {
-                    return '';
-                }
-                return implode(' | ', array_values(json_decode($keyword_category, true)));
-            }, 'category')
-                ->filterable()
-                ->label(__('Kategorie')),
+            'header' => ['', 'CS', 'EN', __('hiko.category')],
+            'rows' => $data->map(function ($kw) {
+                return [
+                    [
+                        'label' => __('hiko.edit'),
+                        'link' => route('keywords.edit', $kw->id),
+                    ],
+                    [
+                        'label' => $kw->getTranslation('name', 'cs'),
+                    ],
+                    [
+                        'label' => $kw->getTranslation('name', 'en'),
+                    ],
+                    [
+                        'label' => $kw->keyword_category ? implode('-', array_values($kw->keyword_category->getTranslations('name'))) : '',
+                    ],
+                ];
+            })->toArray(),
         ];
     }
 }
---
+++ resources/views/livewire/keywords-table.blade.php
@@ -0,0 +1,44 @@
+<div>
+    <form wire:submit.prevent="search" class="w-full p-3 my-8 bg-gray-200 shadow-sm">
+        <div class="flex flex-col flex-wrap gap-4 lg:flex-row">
+            <label>
+                <span class="block text-sm">
+                    CS
+                </span>
+                <x-input wire:model.defer="filters.cs" class="block w-full lg:w-64" type="text" />
+            </label>
+            <label>
+                <span class="block text-sm">
+                    EN
+                </span>
+                <x-input wire:model.defer="filters.en" class="block w-full lg:w-64" type="text" />
+            </label>
+            <label>
+                <span class="block text-sm">
+                    {{ __('hiko.category') }}
+                </span>
+                <x-input wire:model.defer="filters.category" class="block w-full lg:w-64" type="text" />
+            </label>
+            <label>
+                <span class="block text-sm">
+                    {{ __('hiko.order_by') }}
+                </span>
+                <x-select wire:model.defer="filters.order" class="w-full lg:w-64">
+                    <option value="cs">
+                        CS
+                    </option>
+                    <option value="en">
+                        EN
+                    </option>
+                </x-select>
+            </label>
+            <x-button-simple type="button" wire:click="search" class="mt-4">
+                {{ __('hiko.search') }}
+            </x-button-simple>
+        </div>
+    </form>
+    <x-table :tableData="$tableData" />
+    <div class="w-full pl-1 mt-3">
+        {{ $pagination->links() }}
+    </div>
+</div>
