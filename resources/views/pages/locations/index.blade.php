c05d0252b8503420188872744163fb0889cd6993
JarkaP <jarka.p@gmail.com>
2022-02-16 12:01:58 +0100

    hide links for guests

--- resources/views/pages/locations/index.blade.php
+++ resources/views/pages/locations/index.blade.php
@@ -1,8 +1,10 @@
 <x-app-layout :title="$title">
     <x-success-alert />
-    <x-create-link label="{{ __('hiko.new_location') }}" link="{{ route('locations.create') }}" />
-    <a href="{{ route('locations.export') }}" class="inline-block mt-3 text-sm font-semibold">
-        {{ __('hiko.export') }}
-    </a>
+    @can('manage-metadata')
+        <x-create-link label="{{ __('hiko.new_location') }}" link="{{ route('locations.create') }}" />
+        <a href="{{ route('locations.export') }}" class="inline-block mt-3 text-sm font-semibold">
+            {{ __('hiko.export') }}
+        </a>
+    @endcan
     <livewire:locations-table :types="$labels" />
 </x-app-layout>
