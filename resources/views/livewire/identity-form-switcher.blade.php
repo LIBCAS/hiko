c1822ae23d22200fe0047de287d9ae2cbacd7caf
Jarka Pachlová <jarka.p@gmail.com>
2022-02-20 00:01:15 +0100

    add viaf, update spacig

--- resources/views/livewire/identity-form-switcher.blade.php
+++ resources/views/livewire/identity-form-switcher.blade.php
@@ -1,82 +1,87 @@
 <div>
-    <div class="mb-3">
-        <x-label for="type" :value="__('hiko.type')" />
-        <x-select wire:model="identityType" id="type" class="block w-full mt-1" name="type" required>
-            @foreach ($types as $type)
-                <option value="{{ $type }}">
-                    {{ __("hiko.{$type}") }}
-                </option>
-            @endforeach
-        </x-select>
-        @error('type')
-            <div class="text-red-600">{{ $message }}</div>
-        @enderror
-    </div>
-    @if ($identityType === 'person')
-        <div class="space-y-3">
-            <div class="required">
-                <x-label for="surname" :value="__('hiko.surname')" />
-                <x-input id="surname" class="block w-full mt-1" type="text" name="surname"
-                    :value="old('surname', $identity->surname)" required />
-                @error('surname')
-                    <div class="text-red-600">{{ $message }}</div>
-                @enderror
-            </div>
-            <div>
-                <x-label for="forename" :value="__('hiko.forename')" />
-                <x-input id="forename" class="block w-full mt-1" type="text" name="forename"
-                    :value="old('forename', $identity->forename)" />
-                @error('forename')
-                    <div class="text-red-600">{{ $message }}</div>
-                @enderror
-            </div>
-            <div class="flex space-x-6">
-                <div>
-                    <x-label for="birth_year" :value="__('hiko.birth_year')" />
-                    <x-input id="birth_year" class="block w-full mt-1" type="text" name="birth_year"
-                        :value="old('birth_year', $identity->birth_year)" />
-                    @error('birth_year')
-                        <div class="text-red-600">{{ $message }}</div>
-                    @enderror
-                </div>
-                <div>
-                    <x-label for="death_year" :value="__('hiko.death_year')" />
-                    <x-input id="death_year" class="block w-full mt-1" type="text" name="death_year"
-                        :value="old('death_year', $identity->death_year)" />
-                    @error('death_year')
-                        <div class="text-red-600">{{ $message }}</div>
-                    @enderror
-                </div>
-            </div>
-            <div>
-                <x-label for="nationality" :value="__('hiko.nationality')" />
-                <x-input id="nationality" class="block w-full mt-1" type="text" name="nationality"
-                    :value="old('nationality', $identity->nationality)" />
-                @error('nationality')
-                    <div class="text-red-600">{{ $message }}</div>
-                @enderror
-            </div>
-            <div>
-                <x-label for="gender" :value="__('hiko.gender')" />
-                <x-input id="gender" class="block w-full mt-1" type="text" name="gender"
-                    :value="old('gender', $identity->gender)" />
-                @error('gender')
-                    <div class="text-red-600">{{ $message }}</div>
-                @enderror
-            </div>
-            <livewire:repeated-select :items="$selectedProfessions" fieldLabel="{{ __('hiko.profession') }}"
-                fieldKey="profession" route="ajax.professions" />
-            <livewire:repeated-select :items="$selectedCategories" fieldLabel="{{ __('hiko.professions_category') }}"
-                fieldKey="category" route="ajax.professions.category" />
-        </div>
-    @else
-        <div class="required">
-            <x-label for="name" :value="__('Název')" />
-            <x-input id="name" class="block w-full mt-1" type="text" name="name" :value="old('name', $identity->name)"
-                required />
-            @error('name')
+    <div class="space-y-6">
+        <div>
+            <x-label for="type" :value="__('hiko.type')" />
+            <x-select wire:model="identityType" id="type" class="block w-full mt-1" name="type" required>
+                @foreach ($types as $type)
+                    <option value="{{ $type }}">
+                        {{ __("hiko.{$type}") }}
+                    </option>
+                @endforeach
+            </x-select>
+            @error('type')
                 <div class="text-red-600">{{ $message }}</div>
             @enderror
         </div>
-    @endif
+        @if ($identityType === 'person')
+            <div class="space-y-6">
+                <div class="required">
+                    <x-label for="surname" :value="__('hiko.surname')" />
+                    <x-input id="surname" class="block w-full mt-1" type="text" name="surname"
+                        :value="old('surname', $identity->surname)" required />
+                    @error('surname')
+                        <div class="text-red-600">{{ $message }}</div>
+                    @enderror
+                </div>
+                <div>
+                    <x-label for="forename" :value="__('hiko.forename')" />
+                    <x-input id="forename" class="block w-full mt-1" type="text" name="forename"
+                        :value="old('forename', $identity->forename)" />
+                    @error('forename')
+                        <div class="text-red-600">{{ $message }}</div>
+                    @enderror
+                </div>
+                <div class="flex space-x-6">
+                    <div>
+                        <x-label for="birth_year" :value="__('hiko.birth_year')" />
+                        <x-input id="birth_year" class="block w-full mt-1" type="text" name="birth_year"
+                            :value="old('birth_year', $identity->birth_year)" />
+                        @error('birth_year')
+                            <div class="text-red-600">{{ $message }}</div>
+                        @enderror
+                    </div>
+                    <div>
+                        <x-label for="death_year" :value="__('hiko.death_year')" />
+                        <x-input id="death_year" class="block w-full mt-1" type="text" name="death_year"
+                            :value="old('death_year', $identity->death_year)" />
+                        @error('death_year')
+                            <div class="text-red-600">{{ $message }}</div>
+                        @enderror
+                    </div>
+                </div>
+                <div>
+                    <x-label for="nationality" :value="__('hiko.nationality')" />
+                    <x-input id="nationality" class="block w-full mt-1" type="text" name="nationality"
+                        :value="old('nationality', $identity->nationality)" />
+                    @error('nationality')
+                        <div class="text-red-600">{{ $message }}</div>
+                    @enderror
+                </div>
+                <div>
+                    <x-label for="gender" :value="__('hiko.gender')" />
+                    <x-input id="gender" class="block w-full mt-1" type="text" name="gender"
+                        :value="old('gender', $identity->gender)" />
+                    @error('gender')
+                        <div class="text-red-600">{{ $message }}</div>
+                    @enderror
+                </div>
+                <div class="space-y-6">
+                    <livewire:repeated-select :items="$selectedProfessions" fieldLabel="{{ __('hiko.profession') }}"
+                        fieldKey="profession" route="ajax.professions" />
+                    <livewire:repeated-select :items="$selectedCategories"
+                        fieldLabel="{{ __('hiko.professions_category') }}" fieldKey="category"
+                        route="ajax.professions.category" />
+                </div>
+            </div>
+        @else
+            <div class="required">
+                <x-label for="name" :value="__('Název')" />
+                <x-input id="name" class="block w-full mt-1" type="text" name="name"
+                    :value="old('name', $identity->name)" required />
+                @error('name')
+                    <div class="text-red-600">{{ $message }}</div>
+                @enderror
+            </div>
+        @endif
+    </div>
 </div>
--- resources/views/pages/identities/form.blade.php
+++ resources/views/pages/identities/form.blade.php
@@ -1,13 +1,22 @@
 <x-app-layout :title="$title">
     <x-success-alert />
     <form x-data="{ type: '{{ $identity->type ? $identity->type : 'person' }}' }" action="{{ $action }}"
-        method="post" onkeydown="return event.key != 'Enter';" class="max-w-sm space-y-3">
+        method="post" onkeydown="return event.key != 'Enter';" class="max-w-sm space-y-6">
         @csrf
         @isset($method)
             @method($method)
         @endisset
         <livewire:identity-form-switcher :types="$types" :identityType="$selectedType" :identity="$identity"
             :selectedProfessions="$selectedProfessions" :selectedCategories="$selectedCategories" />
+        <div x-data="{ open: false }" class="p-3 bg-gray-200 border rounded-md shadow">
+            <button type="button" @click="open = !open"
+                class="inline-flex items-center font-semibold text-primary hover:underline text-sm">
+                <x-icons.user-group class="h-4 mr-2" /> <span>{{ __('hiko.search_viaf') }}</span>
+            </button>
+            <span x-show="open" x-transition.duration.500ms>
+                <livewire:viaf-search />
+            </span>
+        </div>
         <div>
             <x-label for="viaf_id" value="VIAF ID" />
             <x-input id="viaf_id" class="block w-full mt-1" type="text" name="viaf_id"
