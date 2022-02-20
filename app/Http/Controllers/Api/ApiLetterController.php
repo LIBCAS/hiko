f16bf8e4d72b68a3b4646f9aee734d283b8f012b
Jarka Pachlová <jarka.p@gmail.com>
2022-02-20 20:55:55 +0100

    add filter to letter collection

--- app/Http/Controllers/Api/ApiLetterController.php
+++ app/Http/Controllers/Api/ApiLetterController.php
@@ -3,18 +3,71 @@
 namespace App\Http\Controllers\Api;
 
 use App\Models\Letter;
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\LetterResource;
 use App\Http\Resources\LetterCollection;
 
 class ApiLetterController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $query = Letter::where('status', 'publish')
-            ->paginate();
+        $order = $request->input('order') && in_array($request->input('order'), ['asc', 'desc'])
+            ? $request->input('order')
+            : 'asc';
 
-        return new LetterCollection($query);
+        $limit = $request->input('limit') && ((int) $request->input('limit') > 0) && ((int) $request->input('limit') <= 100)
+            ? (int) $request->input('limit')
+            : 10;
+
+        $with = [];
+
+        if ($request->input('author') || $request->input('recipient')) {
+            $with['identities'] = function ($subquery) {
+                $subquery->select('identities.id', 'name', 'role')
+                    ->whereIn('role', ['author', 'recipient'])
+                    ->orderBy('position');
+            };
+        }
+
+        if ($request->input('origin') || $request->input('destination')) {
+            $with['places'] = function ($subquery) {
+                $subquery->select('places.id', 'name', 'role')
+                    ->whereIn('role', ['origin', 'destination'])
+                    ->orderBy('position');
+            };
+        }
+
+        if ($request->input('keyword')) {
+            $with['keywords'] = function ($subquery) {
+                $subquery->select('keywords.id', 'name');
+            };
+        }
+
+        $query = Letter::with($with)
+            ->where('status', 'publish')
+            ->orderBy('date_computed', $order);
+
+        $query = $this->addScopeByRole($query, $request, 'author', 'identities');
+        $query = $this->addScopeByRole($query, $request, 'recipient', 'identities');
+        $query = $this->addScopeByRole($query, $request, 'origin', 'places');
+        $query = $this->addScopeByRole($query, $request, 'destination', 'places');
+
+        if ($request->input('keyword')) {
+            $query->whereHas('keywords', function ($subquery) use ($request) {
+                $subquery->whereIn('keywords.id', array_map('intval', explode(',', $request->input('keyword'))));
+            });
+        }
+
+        if ($request->input('after')) {
+            $query->whereDate('date_computed', '>=', $request->input('after'));
+        }
+
+        if ($request->input('before')) {
+            $query->whereDate('date_computed', '<=', $request->input('before'));
+        }
+
+        return new LetterCollection($query->paginate($limit));
     }
 
     public function show($uuid)
@@ -40,5 +93,25 @@ class ApiLetterController extends Controller
         ]);
 
         return new LetterResource($letter);
+    }
+
+    protected function sanitizedIds($ids)
+    {
+        $ids = explode(',', $ids);
+        $ids = array_map('trim', $ids);
+        return array_filter($ids);
+    }
+
+    protected function addScopeByRole($query, $request, $role, $type)
+    {
+        if ($request->input($role)) {
+            $query->whereHas($type, function ($subquery) use ($request, $role, $type) {
+                $subquery
+                    ->where('role', $role)
+                    ->whereIn("{$type}.id", array_map('intval', explode(',', $request->input($role))));
+            });
+        }
+
+        return $query;
     }
 }
