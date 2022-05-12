c07a6a9d688b77d325fe82d9a5edbe378f8cfc89
Jarka Pachlová <jarka.p@gmail.com>
2022-05-12 20:31:41 +0200

    chore: set version

--- config/hiko.php
+++ config/hiko.php
@@ -4,7 +4,7 @@ return [
     'geonames_username' => env('GEONAMES_USERNAME', ''),
     'main_character' => env('MAIN_CHARACTER_ID'),
     'metadata_default_locale' => env('METADATA_DEFAULT_LOCALE', 'cs'),
-    'version' => env('APP_VERSION', '1.0.0'),
+    'version' => '1.0.0',
     'show_watermark' => env('SHOW_WATERMARK', true),
 ];
 
