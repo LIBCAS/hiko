5fd02ddcda51cf851754566af50728db1a2e4ab5
Jarka Pachlová <jarka.p@gmail.com>
2022-05-16 00:25:16 +0200

    fix: prevent invalid date

--- app/helpers.php
+++ app/helpers.php
@@ -3,6 +3,10 @@
 if (!function_exists('computeDate')) {
     function computeDate($letter)
     {
+        if ($letter->date_month == 2 && $letter->date_day == 29) {
+            $letter->date_day = 28; // 29th February is not a valid date in mysql
+        }
+
         return implode('-', [
             $letter->date_year ? (string) $letter->date_year : '0001',
             $letter->date_month ? str_pad($letter->date_month, 2, '0', STR_PAD_LEFT) : '01',
