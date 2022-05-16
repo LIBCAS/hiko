7e9ed42c0184a649e40547d6687223b9087f2fbb
JarkaP <jarka.p@gmail.com>
2022-05-16 11:24:06 +0200

    chore: update version

--- config/hiko.php
+++ config/hiko.php
@@ -4,7 +4,7 @@ return [
     'geonames_username' => env('GEONAMES_USERNAME', ''),
     'main_character' => env('MAIN_CHARACTER_ID'),
     'metadata_default_locale' => env('METADATA_DEFAULT_LOCALE', 'cs'),
-    'version' => '1.0.0',
+    'version' => '1.0.1',
     'show_watermark' => env('SHOW_WATERMARK', true),
 ];
 
