0050f7b6d4e95714cb0f590dfd3edd6c7bfe5225
Jarka Pachlová <jarka.p@gmail.com>
2022-05-27 17:05:56 +0200

    chore: update version

--- config/hiko.php
+++ config/hiko.php
@@ -4,7 +4,7 @@ return [
     'geonames_username' => env('GEONAMES_USERNAME', ''),
     'main_character' => env('MAIN_CHARACTER_ID'),
     'metadata_default_locale' => env('METADATA_DEFAULT_LOCALE', 'cs'),
-    'version' => '1.0.1',
+    'version' => '1.0.2',
     'show_watermark' => env('SHOW_WATERMARK', true),
     'public_url' => env('PUBLIC_LETTERS_URL', ''),
 ];
