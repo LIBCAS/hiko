eff501f833ac7c806051c0e912e2008f22d634c5
Jarka Pachlová <jarka.p@gmail.com>
2022-05-27 14:38:33 +0200

    refactor: hide empty links

--- resources/views/components/table.blade.php
+++ resources/views/components/table.blade.php
@@ -19,13 +19,12 @@
                                 @foreach ($row as $item)
                                     <td class="max-w-sm px-3 py-2">
                                         @if (isset($item['component']))
-                                            <x-dynamic-component :component="$item['component']['name']"
-                                                :args="$item['component']['args']" />
-                                        @elseif (isset($item['link']) && isset($item['external']) && $item['external'])
+                                            <x-dynamic-component :component="$item['component']['name']" :args="$item['component']['args']" />
+                                        @elseif (isset($item['link']) && !empty($item['link']) && isset($item['external']) && $item['external'])
                                             <a href="{{ $item['link'] }}" target="_blank" class="hover:underline">
                                                 {{ $item['label'] }} &#10697
                                             </a>
-                                        @elseif (isset($item['link']))
+                                        @elseif (isset($item['link']) && !empty($item['link']))
                                             <a href="{{ $item['link'] }}"
                                                 class="font-semibold text-primary-dark hover:underline">
                                                 {{ $item['label'] }}
