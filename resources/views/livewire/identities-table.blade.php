209b26b9c1086d9bb83189d4ae93abd2a6c3e96e
Jarka Pachlová <jarka.p@gmail.com>
2022-12-20 00:14:52 +0100

    refactor, remember and reset identity table filters

--- resources/views/livewire/identities-table.blade.php
+++ resources/views/livewire/identities-table.blade.php
@@ -1,44 +1,39 @@
 <div>
-    <form wire:submit.prevent="search" wire:keydown.enter="search" class="w-full p-3 my-8 bg-gray-200 shadow-sm">
-        <div class="flex flex-col flex-wrap gap-4 lg:items-end lg:flex-row">
-            <label>
-                <span class="block text-sm">
-                    {{ __('hiko.name') }}
-                </span>
-                <x-input wire:model.defer="filters.name" class="block w-full px-2 text-sm lg:w-64" type="text" />
-            </label>
-            <label>
-                <span class="block text-sm">
-                    {{ __('hiko.type') }}
-                </span>
-                <x-select wire:model.defer="filters.type" class="w-full px-2 text-sm lg:w-64">
-                    <option value="">---</option>
-                    <option value="person">
-                        {{ __('hiko.person') }}
-                    </option>
-                    <option value="institution">
-                        {{ __('hiko.institution') }}
-                    </option>
-                </x-select>
-            </label>
-            <label>
-                <span class="block text-sm">
-                    {{ __('hiko.profession') }}
-                </span>
-                <x-input wire:model.defer="filters.profession" class="block w-full px-2 text-sm lg:w-64" type="text" />
-            </label>
-            <label>
-                <span class="block text-sm">
-                    {{ __('hiko.category') }}
-                </span>
-                <x-input wire:model.defer="filters.category" class="block w-full px-2 text-sm lg:w-64" type="text" />
-            </label>
-            <x-button-simple type="button" wire:click="search" class="py-3">
-                {{ __('hiko.search') }}
-            </x-button-simple>
-        </div>
-    </form>
-    <x-table :tableData="$tableData" />
+    <x-filter-form>
+        <label>
+            <span class="block text-sm">
+                {{ __('hiko.name') }}
+            </span>
+            <x-input wire:model.defer="filters.name" class="block w-full px-2 text-sm lg:w-64" type="text"/>
+        </label>
+        <label>
+            <span class="block text-sm">
+                {{ __('hiko.type') }}
+            </span>
+            <x-select wire:model.defer="filters.type" class="w-full px-2 text-sm lg:w-64">
+                <option value="">---</option>
+                <option value="person">
+                    {{ __('hiko.person') }}
+                </option>
+                <option value="institution">
+                    {{ __('hiko.institution') }}
+                </option>
+            </x-select>
+        </label>
+        <label>
+            <span class="block text-sm">
+               {{ __('hiko.profession') }}
+            </span>
+            <x-input wire:model.defer="filters.profession" class="block w-full px-2 text-sm lg:w-64" type="text"/>
+        </label>
+        <label>
+            <span class="block text-sm">
+               {{ __('hiko.category') }}
+            </span>
+            <x-input wire:model.defer="filters.category" class="block w-full px-2 text-sm lg:w-64" type="text"/>
+        </label>
+    </x-filter-form>
+    <x-table :tableData="$tableData"/>
     <div class="w-full pl-1 mt-3">
         {{ $pagination->links() }}
     </div>
