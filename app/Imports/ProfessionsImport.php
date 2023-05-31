92240551394ef87b9077c7cd9c05cfaa89e5ad5c
NikitaVrnv <redlake18@gmail.com>
2023-05-31 08:28:48 +0200

    Increment primary key from last ID (professions)

--- app/Imports/ProfessionsImport.php
+++ app/Imports/ProfessionsImport.php
@@ -8,30 +8,35 @@ use Illuminate\Support\Facades\Storage;
 
 class ProfessionsImport
 {
-    /**
-     * @throws FileNotFoundException
-     */
     public function import(): string
     {
         if (!Storage::disk('local')->exists('imports/profession.json')) {
             return 'Soubor neexistuje';
         }
 
-        collect(json_decode(Storage::disk('local')->get('imports/profession.json')))
-            ->each(function ($item) {
-                DB::table(
-                    $item->palladio === '0' ? 'professions' : 'profession_categories'
-                )
+        $data = json_decode(Storage::disk('local')->get('imports/profession.json'));
+
+        if (json_last_error() !== JSON_ERROR_NONE) {
+            return 'Chybný formát souboru: ' . json_last_error_msg();
+        }
+
+        foreach ($data as $item) {
+            if (is_object($item)) {
+                DB::table($item->palladio === '0' ? 'professions' : 'profession_categories')
                     ->insert($this->prepare($item));
-            });
+            }
+        }
 
         return 'Import profesí byl úspěšný';
     }
 
     protected function prepare($data): array
     {
+        $lastId = DB::table('professions')->orderByDesc('id')->value('id') ?? 0;
+        $id = $lastId + 1;
+
         return [
-            'id' => $data->id,
+            'id' => $id,
             'created_at' => now(),
             'updated_at' => now(),
             'name' => json_encode([
