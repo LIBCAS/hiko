73347af673d34782b30c1a4447b280ea468e5ccc
NikitaVrnv <redlake18@gmail.com>
2023-05-31 09:51:48 +0200

    Auto-increment primary key (places)

--- app/Imports/PlacesImport.php
+++ app/Imports/PlacesImport.php
@@ -13,24 +13,46 @@ class PlacesImport
      */
     public function import(): string
     {
+
         if (!Storage::disk('local')->exists('imports/place.json')) {
             return 'Soubor neexistuje';
         }
 
-        collect(json_decode(Storage::disk('local')->get('imports/place.json')))
-            ->each(function ($place) {
-                DB::table('places')
-                    ->insert([
+        $json = Storage::disk('local')->get('imports/place.json');
+        $places = json_decode($json);
+
+        if (!$places) {
+            return 'Chyba při dekódování JSON';
+        }
+
+        $importCount = 0;
+        $lastId = DB::table('places')->max('id');
+
+        foreach ($places as $place) {
+            try {
+                $lastId++;
+                $identityId = DB::table('places')->insertGetId([
                         'name' => $place->name,
                         'created_at' => now(),
                         'country' => $place->country,
                         'note' => $place->note,
                         'latitude' => is_numeric($place->latitude) ? $place->latitude : null,
                         'longitude' => is_numeric($place->longitude) ? $place->longitude : null,
-                        'id' => $place->id,
-                    ]);
-            });
+                        'id' => $lastId,
+                ]);
 
-        return 'Import míst byl úspěšný';
+                $importCount++;
+
+            } catch (QueryException $ex) {
+                dump($ex->getMessage());
+            }
+        }
+
+
+        if ($importCount > 0) {
+            return "Import identit byl úspěšný. Počet importovaných záznamů: $importCount";
+        } else {
+            return 'Žádné záznamy nebyly importovány';
+        }
     }
 }
