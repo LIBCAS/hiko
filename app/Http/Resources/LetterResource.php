2b321b9aacc28ce772603438dea9c360e049472a
Nikita Voronov <redlake18@gmail.com>
2024-06-18 14:26:49 +0200

    Ternary operator, reusable methods, signature function improvement
    + Ternary operator added (conditions simplification);
    + Reusable methods are added for repetitive actions;
    + Signature function improvement.

--- app/Http/Resources/LetterResource.php
+++ app/Http/Resources/LetterResource.php
@@ -13,16 +13,7 @@ class LetterResource extends JsonResource
             : $this->getDetailedRecord();
 
         if ($request->media === '1') {
-            $record['media'] = [];
-            foreach ($this->getMedia() as $media) {
-                if ($media->getCustomProperty('status') === 'publish') {
-                    $record['media'][] = [
-                        'thumb' => route('image', [$this, $media, 'size' => 'thumb']),
-                        'full' => route('image', [$this, $media, 'size' => 'full']),
-                        'description' => $media->getCustomProperty('description'),
-                    ];
-                }
-            }
+            $record['media'] = $this->getPublishedMedia();
         }
 
         return $record;
@@ -39,19 +30,11 @@ class LetterResource extends JsonResource
                 'date' => $this->pretty_date,
                 'computed' => $this->date_computed,
             ],
-            'signatures' => collect($this->copies)->pluck('signature')->toArray(),
-            'authors' => isset($identities['author'])
-                ? $identities['author']->pluck('name')->toArray()
-                : [],
-            'recipients' => isset($identities['recipient'])
-                ? $identities['recipient']->pluck('name')->toArray()
-                : [],
-            'origins' => isset($places['origin'])
-                ? $places['origin']->pluck('name')->toArray()
-                : [],
-            'destinations' => isset($places['destination'])
-                ? $places['destination']->pluck('name')->toArray()
-                : [],
+            'signatures' => $this->getSignatures(),
+            'authors' => $this->pluckNames($identities, 'author'),
+            'recipients' => $this->pluckNames($identities, 'recipient'),
+            'origins' => $this->pluckNames($places, 'origin'),
+            'destinations' => $this->pluckNames($places, 'destination'),
         ];
     }
 
@@ -63,79 +46,32 @@ class LetterResource extends JsonResource
         return [
             'name' => $this->name,
             'uuid' => $this->uuid,
-            'dates' => [
-                'date' => $this->pretty_date,
-                'date_range' => $this->date_is_range ? $this->pretty_range_date : '',
-                'date_marked' => $this->date_marked,
-                'date_uncertain' => (bool)$this->date_uncertain,
-                'date_approximate' => (bool)$this->date_approximate,
-                'date_inferred' => (bool)$this->date_inferred,
-                'date_note' => $this->date_note,
-            ],
-            'authors' => [
-                'items' => isset($identities['author'])
-                    ? $identities['author']->map(function ($item) {
-                        return [
-                            'name' => $item->name,
-                            'marked' => $item->pivot->marked,
-                        ];
-                    })->toArray()
-                    : [],
-                'inferred' => (bool)$this->author_inferred,
-                'uncertain' => (bool)$this->author_uncertain,
+            'dates' => $this->getDetailedDates(),
+            'authors' => $this->getDetailedRoleData($identities, 'author', [
+                'inferred' => $this->author_inferred,
+                'uncertain' => $this->author_uncertain,
                 'note' => $this->author_note,
-            ],
-            'recipients' => [
-                'items' => isset($identities['recipient'])
-                    ? $identities['recipient']->map(function ($item) {
-                        return [
-                            'name' => $item->name,
-                            'marked' => $item->pivot->marked,
-                            'salutation' => $item->pivot->salutation,
-                        ];
-                    })->toArray()
-                    : [],
-                'inferred' => (bool)$this->recipient_inferred,
-                'uncertain' => (bool)$this->recipient_uncertain,
+            ]),
+            'recipients' => $this->getDetailedRoleData($identities, 'recipient', [
+                'inferred' => $this->recipient_inferred,
+                'uncertain' => $this->recipient_uncertain,
                 'note' => $this->recipient_note,
-            ],
-            'origins' => [
-                'items' => isset($places['origin'])
-                    ? $places['origin']->map(function ($item) {
-                        return [
-                            'name' => $item->name,
-                            'marked' => $item->pivot->marked,
-                        ];
-                    })->toArray()
-                    : [],
-                'inferred' => (bool)$this->origin_inferred,
-                'uncertain' => (bool)$this->origin_uncertain,
+            ]),
+            'origins' => $this->getDetailedRoleData($places, 'origin', [
+                'inferred' => $this->origin_inferred,
+                'uncertain' => $this->origin_uncertain,
                 'note' => $this->origin_note,
-            ],
-            'destinations' => [
-                'items' => isset($places['destination'])
-                    ? $places['destination']->map(function ($item) {
-                        return [
-                            'name' => $item->name,
-                            'marked' => $item->pivot->marked,
-                        ];
-                    })->toArray()
-                    : [],
-                'inferred' => (bool)$this->destination_inferred,
-                'uncertain' => (bool)$this->destination_uncertain,
+            ]),
+            'destinations' => $this->getDetailedRoleData($places, 'destination', [
+                'inferred' => $this->destination_inferred,
+                'uncertain' => $this->destination_uncertain,
                 'note' => $this->destination_note,
-            ],
+            ]),
             'mentioned' => [
-                'items' => isset($identities['mentioned'])
-                    ? $identities['mentioned']->pluck('name')->toArray()
-                    : [],
+                'items' => $this->pluckNames($identities, 'mentioned'),
                 'note' => $this->people_mentioned_note,
             ],
-            'keywords' => collect($this->keywords)
-                ->map(function ($kw) {
-                    return $kw->getTranslations('name');
-                })
-                ->toArray(),
+            'keywords' => $this->getKeywords(),
             'copies' => (array)$this->copies,
             'related_resources' => (array)$this->related_resources,
             'abstract' => $this->getTranslations('abstract'),
@@ -144,10 +80,77 @@ class LetterResource extends JsonResource
             'content' => $this->content,
             'copyright' => $this->copyright,
             'note' => $this->notes_public,
-            'languages' => collect(explode(';', $this->languages))
-                ->reject(function ($language) {
-                    return empty($language);
-                })->toArray(),
+            'languages' => $this->getLanguages(),
         ];
+    }
+
+    protected function getPublishedMedia(): array
+    {
+        return collect($this->getMedia())->filter(function ($media) {
+            return $media->getCustomProperty('status') === 'publish';
+        })->map(function ($media) {
+            return [
+                'thumb' => route('image', [$this, $media, 'size' => 'thumb']),
+                'full' => route('image', [$this, $media, 'size' => 'full']),
+                'description' => $media->getCustomProperty('description'),
+            ];
+        })->toArray();
+    }
+
+    protected function getSignatures(): array
+    {
+        return collect($this->copies)->pluck('signature')->toArray();
+    }
+
+    protected function pluckNames($collection, $role): array
+    {
+        return isset($collection[$role])
+            ? $collection[$role]->pluck('name')->toArray()
+            : [];
+    }
+
+    protected function getDetailedDates(): array
+    {
+        return [
+            'date' => $this->pretty_date,
+            'date_range' => $this->date_is_range ? $this->pretty_range_date : '',
+            'date_marked' => $this->date_marked,
+            'date_uncertain' => (bool)$this->date_uncertain,
+            'date_approximate' => (bool)$this->date_approximate,
+            'date_inferred' => (bool)$this->date_inferred,
+            'date_note' => $this->date_note,
+        ];
+    }
+
+    protected function getDetailedRoleData($collection, $role, array $additionalData): array
+    {
+        return array_merge([
+            'items' => isset($collection[$role])
+                ? $collection[$role]->map(function ($item) {
+                    return [
+                        'name' => $item->name,
+                        'marked' => $item->pivot->marked,
+                        'salutation' => $item->pivot->salutation ?? null,
+                    ];
+                })->toArray()
+                : [],
+        ], $additionalData);
+    }
+
+    protected function getKeywords(): array
+    {
+        return collect($this->keywords)
+            ->map(function ($kw) {
+                return $kw->getTranslations('name');
+            })
+            ->toArray();
+    }
+
+    protected function getLanguages(): array
+    {
+        return collect(explode(';', $this->languages))
+            ->reject(function ($language) {
+                return empty($language);
+            })->toArray();
     }
 }
