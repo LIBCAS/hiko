490eebeee9863915eb4878b81cce73c3fed983cd
Nikita Voronov <redlake18@gmail.com>
2025-01-03 20:00:42 +0100

    Prevent multiple instances of Alpine running (Livewire v3 has it in the box)

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,3 @@
-// Import 'n init Alpine.js
-import Alpine from 'alpinejs';
-
-window.Alpine = Alpine;
-
-Alpine.start();
-
-// Custom JS
 import './src/select';
 import './src/exportUrl';
 import './src/identityForm';
