c5c2d6c02f09ceb909ef4b1934b4d4fed49bbfbb
Nikita Voronov <redlake18@gmail.com>
2025-02-11 13:11:26 +0100

    MariaDB issue - JSON field querying #223

--- app/Http/Controllers/Ajax/AjaxProfessionController.php
+++ app/Http/Controllers/Ajax/AjaxProfessionController.php
@@ -7,7 +7,9 @@ use App\Http\Controllers\Controller;
 use App\Models\Profession;
 use App\Models\GlobalProfession;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Log;
 use Stancl\Tenancy\Facades\Tenancy;
+use Illuminate\Support\Facades\DB;
 
 class AjaxProfessionController extends Controller
 {
@@ -35,20 +37,35 @@ class AjaxProfessionController extends Controller
 
         $results = $localProfessions->toArray();
 
-        // Fetch global professions within central context
+        // Fetch global professions within central context (MySQL/MariaDB Compatible)
         Tenancy::central(function () use ($search, &$results) {
-            $globalProfessions = GlobalProfession::whereRaw("LOWER(JSON_UNQUOTE(name->'$.en')) like ?", ['%' . $search . '%'])
-                ->select('id', 'name')
-                ->take(25)
-                ->get()
-                ->map(function ($profession) {
-                    return [
-                        'id' => 'global-' . $profession->id,
-                        'value' => 'global-' . $profession->id,
-                        'label' => "{$profession->name} (Global)",
-                    ];
-                });
-            $results = array_merge($results, $globalProfessions->toArray());
+            try {
+                // Check if the database is MariaDB
+                $dbVersion = DB::select("SELECT VERSION() AS version")[0]->version;
+                $isMariaDB = Str::contains(strtolower($dbVersion), 'mariadb');
+
+                // Use JSON_VALUE() for MariaDB, otherwise use MySQL's JSON path syntax
+                $jsonQuery = $isMariaDB
+                    ? "LOWER(JSON_VALUE(name, '$.en')) like ?"
+                    : "LOWER(JSON_UNQUOTE(name->'$.en')) like ?";
+
+                $globalProfessions = GlobalProfession::whereRaw($jsonQuery, ['%' . $search . '%'])
+                    ->select('id', 'name')
+                    ->take(25)
+                    ->get()
+                    ->map(function ($profession) {
+                        return [
+                            'id' => 'global-' . $profession->id,
+                            'value' => 'global-' . $profession->id,
+                            'label' => "{$profession->name} (Global)",
+                        ];
+                    });
+
+                $results = array_merge($results, $globalProfessions->toArray());
+
+            } catch (\Exception $e) {
+                Log::error("Error fetching global professions: " . $e->getMessage());
+            }
         });
 
         return $results;
