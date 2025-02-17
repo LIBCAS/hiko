76348483c3569405ca2cf6e16efa23209aa1bb79
Nikita Voronov <redlake18@gmail.com>
2025-02-18 00:02:35 +0100

    Letter export options

--- resources/views/pages/letters/index.blade.php
+++ resources/views/pages/letters/index.blade.php
@@ -14,6 +14,16 @@
                         class="block w-full px-2 py-1 text-sm text-left text-gray-700 hover:bg-gray-100">
                         {{ __('hiko.export_selected') }}
                     </a>
+                    @if ($mainCharacter)
+                        <a href="{{ route('letters.export.palladio.character', ['role' => 'author']) }}"
+                            class="block w-full px-2 py-1 text-sm text-left text-gray-700 hover:bg-gray-100">
+                            {{ __('hiko.letters_from', ['name' => $mainCharacter]) }}
+                        </a>
+                        <a href="{{ route('letters.export.palladio.character', ['role' => 'recipient']) }}"
+                            class="block w-full px-2 py-1 text-sm text-left text-gray-700 hover:bg-gray-100">
+                            {{ __('hiko.letters_to', ['name' => $mainCharacter]) }}
+                        </a>
+                    @endif
                 </div>
             </x-dropdown>
         </div>
